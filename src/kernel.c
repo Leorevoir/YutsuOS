d90d588e263bbe5946545467dc2b60dddf39e613
Leorevoir <leo.quinzler@epitech.eu>
2025-09-18 14:04:58 +0200

    add: kernel.c (example for base compilation tests)

---
+++ src/kernel.c
@@ -0,0 +1,11 @@
+// #include <YutsuOS/Macros.h>
+// #include <YutsuOS/VGA.h>
+
+#include <stdio.h>
+
+int main(void)
+{
+    // yutsuos_vga_clear_screen();
+    printf("hello !\n");
+    return 0;
+}
