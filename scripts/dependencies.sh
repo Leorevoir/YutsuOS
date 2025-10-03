57e09f7a1375a07a99f8e0f88fd0392c53044093
Leorevoir <leo.quinzler@epitech.eu>
2025-10-03 14:06:49 +0200

    remove: sudo apt upgrade because its too long on github workflows

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
+#
+# CI/CD script to install dependencies on Ubuntu for the gitaction workflow
+#
+
 set -e
 
 function install_dependencies()
 {
     local packages="cmake gcc ninja-build nasm make qemu-system-x86 qemu-utils grub-pc-bin grub-common grub2-common xorriso"
 
-    sudo apt update && sudo apt upgrade -y
+    sudo apt update
     sudo apt install -y $packages
 }
 
