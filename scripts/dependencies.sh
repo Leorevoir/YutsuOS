68229133387c090588d424889046a344f6d953c6
Leorevoir <leo.quinzler@epitech.eu>
2025-10-03 14:03:45 +0200

    remove: sudo from the dep installer script

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -6,8 +6,8 @@ function install_dependencies()
 {
     local packages="cmake gcc ninja-build nasm make qemu-system-x86 qemu-utils grub-pc-bin grub-common grub2-common xorriso"
 
-    sudo apt update && sudo apt upgrade -y
-    sudo apt install -y $packages
+    apt update && sudo apt upgrade -y
+    apt install -y $packages
 }
 
 install_dependencies "$@"
