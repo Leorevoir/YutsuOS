01cf3e78bb517ff8a23bfb232aa09e7ac1054372
Leorevoir <leo.quinzler@epitech.eu>
2025-10-03 14:07:58 +0200

    add: additional packages mtools dosfstools

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -8,10 +8,10 @@ set -e
 
 function install_dependencies()
 {
-    local packages="cmake gcc ninja-build nasm make qemu-system-x86 qemu-utils grub-pc-bin grub-common grub2-common xorriso"
+    local packages="cmake gcc g++ ninja-build nasm make qemu-system-x86 qemu-utils grub-pc-bin grub-common grub2-common xorriso mtools dosfstools"
 
     sudo apt update
     sudo apt install -y $packages
 }
 
-install_dependencies "$@"
+install_dependencies
