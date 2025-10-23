20e2fcb5233673c9c08619f785e02ebccbbb4d49
Leorevoir <leo.quinzler@epitech.eu>
2025-10-23 15:39:49 +0200

    add: 64kb ram of memory

--- src/boot.asm
+++ src/boot.asm
@@ -1,21 +1,29 @@
 bits 32
 
 section .multiboot
-    align 4
-    dd 0x1BADB002               ; bootloader magic number
-    dd 0x00                     ; flags
-    dd - (0x1BADB002 + 0x00)    ; checksum
+align   4
+dd      0x1BADB002; bootloader magic number
+dd      0x00; flags
+dd      - (0x1BADB002 + 0x00); checksum
 
 section .text
-    global _start
-    extern main                 ; kernel.c
+global  _start
+extern  main; kernel.c
 
 _start:
-    cli                         ; block interrupts
-    mov esp, stack_space        ; set stack pointer
-    call main                   ; call int main(void) from kernel.c
-    hlt                         ; halt the CPU
+	cli  ; block interrupts
+	mov  esp, stack_space; set stack pointer
+	call main; call int main(void) from kernel.c
+	hlt  ; halt the CPU
 
-section .bss
-resb 8192                       ; stack size: ~8kb
+	section .bss
+	resb    8192; stack size: ~8kb
+
 stack_space:
+	global heap_start
+
+heap_start:
+	resb   65536; reserve 64KB for the heap for now
+	global heap_end
+
+heap_end:
