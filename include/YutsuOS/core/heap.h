c126e28dbbfdecd0cacfb3e213a0d02935d4a42c
Leorevoir <leo.quinzler@epitech.eu>
2025-10-23 15:39:29 +0200

    add: heap.h

--- include/YutsuOS/core/heap.h
+++ include/YutsuOS/core/heap.h
@@ -3,28 +3,40 @@
 
 #include <YutsuOS/std/types.h>
 
-#define YUTSUOS_CORE_HEAP_ALIGNMENT 8
-#define YUTSUOS_CORE_HEAP_MAX_BINS 128
+#define YUTSUOS_CORE_HEAP_MAGIC_USED 0xDEADBEEF
+#define YUTSUOS_CORE_HEAP_MAGIC_FREE 0xCAFEBABE
+#define YUTSUOS_CORE_HEAP_HEADER_SIZE sizeof(BlockHeader)
 
-typedef struct Block Block;
-typedef struct Page Page;
-
-struct Block
+/**
+ * @struct BlockHeader
+ * @brief Header for each memory block in the heap.
+ *
+ * This header is placed just before the allocated memory block and stores
+ * metadata, including a magic number for validation, its size, and a pointer
+ * to the next block in the free list.
+ */
+typedef struct BlockHeader
 {
-    Block *next;
-    Block *prev;
-    Page *page;
-    u64 size;
-};
+    u32 magic;                //<<< Magic number: 0xDEADBEEF for used, 0xCAFEBABE for free
+    u32 size;                 //<<< Size of the block, including the header
+    struct BlockHeader *next; //<<< Pointer to the next block in the free list
+} BlockHeader;
 
-struct Page
-{
-    Page *next;
-    Page *prev;
-    u64 size;
-    Block free;
-};
+/**
+ * @brief Initializes the kernel heap.
+ */
+void __yutsuos_core_heap_init(void);
 
-void __yutsuos_core_heap_acquire(void);
+/**
+ * @brief Gets the head of the free list.
+ * @return Pointer to the head of the free list.
+ */
+BlockHeader *__yutsuos_core_heap_get_free_list_head(void);
+
+/**
+ * @brief Sets the head of the free list.
+ * @param head Ponter to the new head of the free list.
+ */
+void __yutsuos_core_heap_set_free_list_head(BlockHeader *head);
 
 #endif /* YUTSUOS_CORE_HEAP_H */
