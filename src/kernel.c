bb8a44d61c766b5fd6a67fb847c6fa68208fbbe8
Leorevoir <leo.quinzler@epitech.eu>
2025-10-23 14:30:11 +0200

    remove: panic test from kernel.c

--- src/kernel.c
+++ src/kernel.c
@@ -1,4 +1,3 @@
-// #include "YutsuOS/core/panic.h"
 #include <YutsuOS/core/kernel.h>
 #include <YutsuOS/macros.h>
 #include <YutsuOS/yutsh/yutsh.h>
@@ -6,7 +5,6 @@
 int main(void)
 {
     __yutsuos_core_kernel_init();
-    // panic("Kernel panic test");
     yutsu_shell();
     return YUTSUOS_SUCCESS;
 }
